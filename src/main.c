485237596831660db4250ed55ab488d9cba00cbe
outsiderice <arantxamagnelli@gmail.com>
2024-11-21 12:19:58 +0100

    wrting render_minimap

--- src/main.c
+++ src/main.c
@@ -8,11 +8,13 @@
 
 #define BPP sizeof(int32_t)
 
-int	worldMap[3][3] =
+int	worldMap[5][5] =
 {
-	{111},
-	{101},
-	{111}
+	{11111},
+	{10001},
+	{10001},
+	{10001},
+	{11111}
 };
 
 // Exit the program as failure.
@@ -22,7 +24,7 @@ static void ft_error(void)
 	exit(EXIT_FAILURE);
 }
 
-// 'Encodes' four individual bytes into an int.
+// 'Encodes' four individual bytes into an int. Aka sets color to one int
 int get_rgba(int r, int g, int b, int a)
 {
     return (r << 24 | g << 16 | b << 8 | a);
@@ -34,32 +36,57 @@ static void ft_hook(void* param)
 	//controls go here
 }
 
-int32_t	main(void)
+void	render_minimap(mlx_image_t *minimap)
 {
-	//parsing stuff
+	int	height;
+	int	width;
+	int	wall_color;
+	int	floor_color;
+	
+	height = 0;
+	floor_color = get_rgba(255, 255, 255, 255);
+	wall_color = get_rgba(0, 0, 0, 0);
+	while (height < HEIGHT)
+	{
+		width = 0;
+		while (width < WIDTH)
+		{
+			width++;
+		}
+		height++;
+	}
+	return ;
+}
+
+void	cub3d()
+{
 	mlx_t* mlx = mlx_init(WIDTH, HEIGHT, "Cub3d", false);
 	if (!mlx)
 		ft_error();
+	mlx_image_t* minimap = mlx_new_image(mlx, 64, 40);
 
-	/* Do stuff */
-
-	// Create and display the image.
-	mlx_image_t* img = mlx_new_image(mlx, WIDTH, HEIGHT);
-	
-	//sets color to white by setting each channel of each pixel to 255
-	ft_memset(img->pixels, 255, img->width * img->height * BPP);
+	render_minimap(minimap);
 	
 	//Draw image at coordinate (0,0)
 	if (!img || (mlx_image_to_window(mlx, img, 0, 0) < 0))
 		ft_error();
-
-	// Even after the image is being displayed, we can still modify the buffer.
-	mlx_put_pixel(img, 0, 0, 0xFF0000FF);
 
 	// Register a hook and pass mlx as an optional param.
 	// NOTE: Do this before calling mlx_loop!
 	mlx_loop_hook(mlx, ft_hook, mlx);
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
+}
+
+int32_t	main(void)
+{
+	/*parsing stuff goes here
+ * 		If there's no errors the following is saved to a struct to be passed to cub3d:
+ *		- Texture paths 			-> strings
+ *		- Map						-> two dimensional array
+ *		- Colors for floor and sky	-> pass rgba to get_rgba and save the returned int for sky and floor
+ * */
+
+	cub3d(); //for now void, later it will be passed the parsed info from the .cub file
 	return (EXIT_SUCCESS);
 }
