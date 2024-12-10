0dd15dc59f69b7e235f5c4519224ce1121fead55
Ricardo Pocaterra Meza <rpocater@car8s2.42barcelona.com>
2024-12-10 13:50:56 +0100

    utils parse done

--- src/utils_parse.c
+++ src/utils_parse.c
@@ -6,7 +6,7 @@
 /*   By: rpocater <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 11:19:44 by rpocater          #+#    #+#             */
-/*   Updated: 2024/12/09 14:28:16 by rpocater         ###   ########.fr       */
+/*   Updated: 2024/12/10 13:49:55 by rpocater         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,44 +15,38 @@
 void	init_map_info(t_map_info *map_info)
 {
 	map_info->north = NULL;
-        map_info->south = NULL;
-        map_info->west = NULL;
-        map_info->east = NULL;
-        map_info->floor[0] = -1;
-        map_info->ceiling[0] = -1;
+	map_info->south = NULL;
+	map_info->west = NULL;
+	map_info->east = NULL;
+	map_info->floor[0] = -1;
+	map_info->ceiling[0] = -1;
 	map_info->floor[1] = -1;
-        map_info->ceiling[1] = -1;
+	map_info->ceiling[1] = -1;
 	map_info->floor[2] = -1;
-        map_info->ceiling[2] = -1;
+	map_info->ceiling[2] = -1;
 	map_info->map = NULL;
 }
 
-void    free_dpint(int **com, int map_lines)
+void	free_dpint(int **com, int map_lines)
 {
-        int     i;
+	int	i;
 
-        i = 0;
-        while (i < map_lines)
-        {
-                free(com[i]);
+	i = 0;
+	while (i < map_lines)
+	{
+		free(com[i]);
 		i++;
-        }
+	}
 	free(com);
-        return ;
+	return ;
 }
 
-void    free_map_info(t_map_info *map_info)
+void	free_map_info(t_map_info *map_info)
 {
-        free(map_info->north);
-        free(map_info->south);
-        free(map_info->west);
-        free(map_info->east);
-        //map_info->floor[0] = -1;
-        //map_info->ceiling[0] = -1;
-        //map_info->floor[1] = -1;
-        //map_info->ceiling[1] = -1;
-        //map_info->floor[2] = -1;
-        //map_info->ceiling[2] = -1;
+	free(map_info->north);
+	free(map_info->south);
+	free(map_info->west);
+	free(map_info->east);
 }
 
 void	print_map_info(t_map_info map_info, int map_len, int map_lines)
@@ -63,15 +57,15 @@ void	print_map_info(t_map_info map_info, int map_len, int map_lines)
 	i = 0;
 	x = 0;
 	printf("North: %s\n", map_info.north);
-        printf("South: %s\n", map_info.south);
-        printf("East: %s\n", map_info.east);
-        printf("West: %s\n", map_info.west);
-        printf("Floor RGB: %d, %d, %d\n", map_info.floor[0], map_info.floor[1], map_info.floor[2]);
-        printf("Ceiling RGB: %d, %d, %d\n", map_info.ceiling[0], map_info.ceiling[1], map_info.ceiling[2]);
+	printf("South: %s\n", map_info.south);
+	printf("East: %s\n", map_info.east);
+	printf("West: %s\n", map_info.west);
+	printf("Floor RGB: %d, %d, %d\n", map_info.floor[0], map_info.floor[1], map_info.floor[2]);
+	printf("Ceiling RGB: %d, %d, %d\n", map_info.ceiling[0], map_info.ceiling[1], map_info.ceiling[2]);
 	while (i < map_lines)
 	{
 		x = 0;
-		while(x < map_len)
+		while (x < map_len)
 		{
 			if ((map_info.map[i][x]) == 0)
 				printf("0");
@@ -88,26 +82,26 @@ void	print_map_info(t_map_info map_info, int map_len, int map_lines)
 	}
 }
 
-int     ft_isspace(int c)
+int	ft_isspace(int c)
 {
-        if ((c >= 9 && c <= 13) || c == ' ')
-                return (0);
-        return (1);
+	if ((c >= 9 && c <= 13) || c == ' ')
+		return (0);
+	return (1);
 }
 
-void    free_dpchar(char **com)
+void	free_dpchar(char **com)
 {
-        int     i;
+	int	i;
 
-        i = 0;
-        if (com != NULL)
-        {
-                while (com[i] != NULL)
-                {
-                        free(com[i]);
-                        i++;
-                }
-                free(com);
-        }
-        return ;
+	i = 0;
+	if (com != NULL)
+	{
+		while (com[i] != NULL)
+		{
+			free(com[i]);
+			i++;
+		}
+		free(com);
+	}
+	return ;
 }
