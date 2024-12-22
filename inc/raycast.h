45f0e57827dd16b4da51b9fc1f852d7e44c55032
A.M.S <99796232+outsiderice@users.noreply.github.com>
2024-12-22 12:43:14 +0000

    adding vars to raycast struct

--- inc/raycast.h
+++ inc/raycast.h
@@ -5,6 +5,11 @@ typedef struct s_raycast
 {
     double  delta_x;
     double  delta_y;
+    double  perp_wall_dist;
+    int     step_x;
+    int     step_y;
+    int     hit;
+    int     side;
 } t_raycast;
 
 #endif
--- src/raycast.c
+++ src/raycast.c
@@ -1,20 +1,25 @@
 #include "render.h"
 #include "raycast.h"
 
-//distance from one x or y to the next x or y
-void	calculate_delta(double ray_x, double ray_y, t_map *map)
+void	calculate_side_dist()
+{
+
+}
+
+//distance from one x or y side to the next x or y side
+void	calculate_delta(double raydir_x, double raydir_y, t_raycast r)
 {
 	double	delta_x;
 	double	delta_y;
 
-	if (ray_x == 0)
-		delta_x = 1e30;
+	if (raydir_x == 0)
+		r.delta_x = 1e30;
 	else
-		delta_x = abs(1 / ray_x);
-	if (ray_y == 0)
-		delta_y = 1e30;
+		r.delta_x = abs(1 / raydir_x);
+	if (raydir_y == 0)
+		r.delta_y = 1e30;
 	else
-		delta_y = abs(1 / ray_y);
+		r.delta_y = abs(1 / raydir_y);
 	return;
 }
 
@@ -22,17 +27,19 @@ void	raycast(t_map *map, mlx_image_t *img, t_player *player)
 {
 	int	x;
 	double	camera_x;
-	double	ray_x;
-	double	ray_y;
+	double	raydir_x;
+	double	raydir_y;
 	t_raycast	r;
 	
 	x = 0;
+	//update map pos with player pos before raycasting here?
 	while (x < WIDTH)
 	{
 		camera_x = 2 * x / (double)WIDTH - 1;
-		ray_x = player->dir_x + player->plane_x * camera_x;
-		ray_y = player->dir_y + player->plane_y * camera_x;
-		calculate_distance(ray_x, ray_y, map);
+		raydir_x = player->dir_x + player->plane_x * camera_x;
+		raydir_y = player->dir_y + player->plane_y * camera_x;
+		calculate_delta(raydir_x, raydir_y, r);
+		calculate_side_dist();
 		x++;
 	}
 }
