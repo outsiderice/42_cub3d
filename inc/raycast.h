2aafa4155eab3893cf6840b855adfedb3a008c22
A.M.S <99796232+outsiderice@users.noreply.github.com>
2024-12-22 15:55:36 +0000

    sidedist and step

--- inc/raycast.h
+++ inc/raycast.h
@@ -6,6 +6,8 @@ typedef struct s_raycast
     double  delta_x;
     double  delta_y;
     double  perp_wall_dist;
+    double  sidedist_x;
+    double  sidedist_y;
     int     step_x;
     int     step_y;
     int     hit;
--- src/raycast.c
+++ src/raycast.c
@@ -1,9 +1,28 @@
 #include "render.h"
 #include "raycast.h"
 
-void	calculate_side_dist()
+void	calc_sidedist_and_step(double raydir_x, double raydir_y, t_raycast r, t_cub *cub)
 {
-
+	if (raydir_x < 0)
+	{
+		r.step_x = -1;
+		r.sidedist_x = (cub->player->pos_x - cub->map->pos_x) * r.delta_x;
+	}
+	else
+	{
+		r.step_x = 1;
+		r.sidedist_x = (cub->map->pos_x + 1.0 - cub->player->pos_x) * r.delta_x;
+	}
+	if (raydir_y < 0)
+	{
+		r.step_y = -1;
+		r.sidedist_y = (cub->player->pos_y - cub->map->pos_y) * r.delta_y;
+	}
+	else
+	{
+		r.step_y = 1;
+		r.sidedist_y = (cub->map->pos_y + 1.0 - cub->player->pos_y) * r.delta_y;
+	}
 }
 
 //distance from one x or y side to the next x or y side
@@ -23,7 +42,7 @@ void	calculate_delta(double raydir_x, double raydir_y, t_raycast r)
 	return;
 }
 
-void	raycast(t_map *map, mlx_image_t *img, t_player *player)
+void	raycast(t_cub *cub, mlx_image_t *img)
 {
 	int	x;
 	double	camera_x;
@@ -36,10 +55,10 @@ void	raycast(t_map *map, mlx_image_t *img, t_player *player)
 	while (x < WIDTH)
 	{
 		camera_x = 2 * x / (double)WIDTH - 1;
-		raydir_x = player->dir_x + player->plane_x * camera_x;
-		raydir_y = player->dir_y + player->plane_y * camera_x;
+		raydir_x = cub->player->dir_x + cub->player->plane_x * camera_x;
+		raydir_y = cub->player->dir_y + cub->player->plane_y * camera_x;
 		calculate_delta(raydir_x, raydir_y, r);
-		calculate_side_dist();
+		calc_sidedist_and_step(raydir_x, raydir_y, r, cub);
 		x++;
 	}
 }
