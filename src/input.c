fedeef6668e5c898a1f57deb692d0936cbaf8009
outsiderice <arantxamagnelli@gmail.com>
2025-01-09 13:25:03 +0100

    move ok

--- src/input.c
+++ src/input.c
@@ -17,13 +17,8 @@ void	close_cub(t_cub *cub)
 	exit(EXIT_SUCCESS);
 }
 
-void	update_player_pos(t_cub *cub)
+void	update_player_pos(t_cub *cub, int x, int y)
 {
-	int	x;
-	int	y;
-
-	x = round(cub->player->dir_x);
-	y = round(cub->player->dir_y);
 	if (cub->map->m[(int)cub->player->pos_y - y][(int)cub->player->pos_x + x] != FLOOR)
 		return ;
 	cub->map->m[(int)cub->player->pos_y][(int)cub->player->pos_x] = '0';
@@ -32,7 +27,7 @@ void	update_player_pos(t_cub *cub)
 	cub->map->m[(int)cub->player->pos_y][(int)cub->player->pos_x] = 'N';
 }
 
-#define ROT_ANGLE 5
+#define ROT_ANGLE 1
 
 void	rotate_vector(double x, double y, double *new_x, double *new_y)
 {
@@ -48,22 +43,22 @@ void	update_player_dir(t_player *player)
 
 void ft_hook(void *param)
 {
-	t_cub *cub;
+	t_cub *c;
 	
-	cub = param;
-	if (mlx_is_key_down(cub->mlx, MLX_KEY_ESCAPE))
-		close_cub(cub);
-	if (mlx_is_key_down(cub->mlx, MLX_KEY_W))
-		update_player_pos(cub);
-	if (mlx_is_key_down(cub->mlx, MLX_KEY_S))
-		update_player_pos(cub);
-	if (mlx_is_key_down(cub->mlx, MLX_KEY_A))
-		update_player_pos(cub);
-	if (mlx_is_key_down(cub->mlx, MLX_KEY_D))
-		update_player_pos(cub);
-	if (mlx_is_key_down(cub->mlx, MLX_KEY_LEFT))
-		update_player_dir(cub->player);
-	if (mlx_is_key_down(cub->mlx, MLX_KEY_RIGHT))
-		update_player_dir(cub->player);
-	render(cub);
+	c = param;
+	if (mlx_is_key_down(c->mlx, MLX_KEY_ESCAPE))
+		close_cub(c);
+	if (mlx_is_key_down(c->mlx, MLX_KEY_W))
+		update_player_pos(c, round(c->player->dir_x), round(c->player->dir_y));
+	if (mlx_is_key_down(c->mlx, MLX_KEY_S))
+		update_player_pos(c, -round(c->player->dir_x), -round(c->player->dir_y));
+	if (mlx_is_key_down(c->mlx, MLX_KEY_A))
+		update_player_pos(c,round(c->player->dir_y), round(c->player->dir_x));
+	if (mlx_is_key_down(c->mlx, MLX_KEY_D))
+		update_player_pos(c,round(c->player->dir_y), -round(c->player->dir_x));
+	if (mlx_is_key_down(c->mlx, MLX_KEY_LEFT))
+		update_player_dir(c->player);
+	if (mlx_is_key_down(c->mlx, MLX_KEY_RIGHT))
+		update_player_dir(c->player);
+	render(c);
 }	
