40c2c892c53d805ab5ec6fdb663c62ef4f67db2c
outsiderice <arantxamagnelli@gmail.com>
2025-01-13 09:16:15 +0100

    on the way

--- src/movements.c
+++ src/movements.c
@@ -1,5 +1,6 @@
 #include "movements.h"
 #include "render.h"
+#include <stdio.h>
 
 void	move_up(t_cub *c)
 {
@@ -7,12 +8,16 @@ void	move_up(t_cub *c)
 	int		pos_x;
 	int		pos_y;
 	
+	printf("up\n");
 	pos_x = (int)c->player->pos_x;
 	pos_y = (int)c->player->pos_y;
 	mv_speed = c->mlx->delta_time * SPEED;
-	if (c->map->m[pos_y][pos_x + (int)(c->player->dir_x * mv_speed)] == FLOOR)
+	if (c->map->m[pos_y][pos_x + (int)(c->player->dir_x * mv_speed)] == '0')
+	{
+		printf("up I said\n");
 		c->player->pos_x += c->player->dir_x * mv_speed;
-	if (c->map->m[pos_y - (int)(c->player->dir_y * mv_speed)][pos_x] == FLOOR)
+	}
+	if (c->map->m[pos_y - (int)(c->player->dir_y * mv_speed)][pos_x] != '0')
 		c->player->pos_y -= c->player->dir_y * mv_speed;
 }
 
@@ -25,9 +30,9 @@ void	move_down(t_cub *c)
 	pos_x = (int)c->player->pos_x;
 	pos_y = (int)c->player->pos_y;
 	mv_speed = c->mlx->delta_time * SPEED;
-	if (c->map->m[pos_y][pos_x - (int)(c->player->dir_x * mv_speed)] == FLOOR)
+	if (c->map->m[pos_y][pos_x - (int)(c->player->dir_x * mv_speed)] != '0')
 		c->player->pos_x -= c->player->dir_x * mv_speed;
-	if (c->map->m[pos_y + (int)(c->player->dir_y * mv_speed)][pos_x] == FLOOR)
+	if (c->map->m[pos_y + (int)(c->player->dir_y * mv_speed)][pos_x] != '0')
 		c->player->pos_y += c->player->dir_y * mv_speed;
 }
 
@@ -40,9 +45,9 @@ void	move_left(t_cub *c)
 	pos_x = (int)c->player->pos_x;
 	pos_y = (int)c->player->pos_y;
 	mv_speed = c->mlx->delta_time * SPEED;
-	if (c->map->m[pos_y][pos_x + (int)(c->player->dir_y * mv_speed)] == FLOOR)
+	if (c->map->m[pos_y][pos_x + (int)(c->player->dir_y * mv_speed)] != '0')
 		c->player->pos_x += c->player->dir_y * mv_speed;
-	if (c->map->m[pos_y - (int)(c->player->dir_x * mv_speed)][pos_x] == FLOOR)
+	if (c->map->m[pos_y - (int)(c->player->dir_x * mv_speed)][pos_x] != '0')
 		c->player->pos_y -= c->player->dir_x * mv_speed;
 }
 
@@ -55,8 +60,8 @@ void	move_right(t_cub *c)
 	pos_x = (int)c->player->pos_x;
 	pos_y = (int)c->player->pos_y;
 	mv_speed = c->mlx->delta_time * SPEED;
-	if (c->map->m[pos_y][pos_x - (int)(c->player->dir_y * mv_speed)] == FLOOR)
+	if (c->map->m[pos_y][pos_x - (int)(c->player->dir_y * mv_speed)] != '0')
 		c->player->pos_x += c->player->dir_y * mv_speed;
-	if (c->map->m[pos_y + (int)(c->player->dir_x * mv_speed)][pos_x] == FLOOR)
+	if (c->map->m[pos_y + (int)(c->player->dir_x * mv_speed)][pos_x] != '0')
 		c->player->pos_y += c->player->dir_x * mv_speed;
 }
