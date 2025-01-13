a5ed759bdde4e513aea45b1356637fbd3d96f10d
outsiderice <arantxamagnelli@gmail.com>
2025-01-13 07:54:43 +0100

    written up down, next left rigth

--- src/movements.c
+++ src/movements.c
@@ -3,36 +3,60 @@
 
 void	move_up(t_cub *c)
 {
-	double mv_speed;
-
+	double	mv_speed;
+	int		pos_x;
+	int		pos_y;
+	
+	pos_x = (int)c->player->pos_x;
+	pos_y = (int)c->player->pos_y;
 	mv_speed = c->mlx->delta_time * SPEED;
-	c->player->pos_x += c->player->dir_x * mv_speed;
-	c->player->pos_y += c->player->dir_y * mv_speed;
+	if (c->map->m[pos_y][pos_x + c->player->dir_x * mv_speed] == FLOOR)
+		c->player->pos_x += c->player->dir_x * mv_speed;
+	if (c->map->m[pos_y + c->player->dir_y * mv_speed][pos_x] == FLOOR)
+		c->player->pos_y += c->player->dir_y * mv_speed;
 }
 
 void	move_down(t_cub *c)
 {
-	double mv_speed;
-
+	double	mv_speed;
+	int		pos_x;
+	int		pos_y;
+	
+	pos_x = (int)c->player->pos_x;
+	pos_y = (int)c->player->pos_y;
 	mv_speed = c->mlx->delta_time * SPEED;
-	c->player->pos_x -= c->player->dir_x * mv_speed;
-	c->player->pos_y -= c->player->dir_y * mv_speed;
+	if (c->map->m[pos_y][pos_x - c->player->dir_x * mv_speed] == FLOOR)
+		c->player->pos_x -= c->player->dir_x * mv_speed;
+	if (c->map->m[pos_y - c->player->dir_y * mv_speed][pos_x] == FLOOR)
+		c->player->pos_y -= c->player->dir_y * mv_speed;
 }
 
 void	move_left(t_cub *c)
 {
-	double mv_speed;
+	double	mv_speed;
+	int		pos_x;
+	int		pos_y;
 
+	pos_x = (int)c->player->pos_x;
+	pos_y = (int)c->player->pos_y;
 	mv_speed = c->mlx->delta_time * SPEED;
-	c->player->pos_x += c->player->dir_y * mv_speed;
-	c->player->pos_y -= c->player->dir_x * mv_speed;
+	if (c->map->m[pos_y][pos_x - c->player->dir_x * mv_speed] == FLOOR)
+		c->player->pos_x += c->player->dir_y * mv_speed;
+	if (c->map->m[pos_y - c->player->dir_y * mv_speed][pos_x] == FLOOR)
+		c->player->pos_y -= c->player->dir_x * mv_speed;
 }
 
 void	move_right(t_cub *c)
 {
-	double mv_speed;
+	double	mv_speed;
+	int		pos_x;
+	int		pos_y;
 
+	pos_x = (int)c->player->pos_x;
+	pos_y = (int)c->player->pos_y;
 	mv_speed = c->mlx->delta_time * SPEED;
-	c->player->pos_x += c->player->dir_y * mv_speed;
-	c->player->pos_y -= -c->player->dir_x * mv_speed;
+	if (c->map->m[pos_y][pos_x - c->player->dir_x * mv_speed] == FLOOR)
+		c->player->pos_x += c->player->dir_y * mv_speed;
+	if (c->map->m[pos_y - c->player->dir_y * mv_speed][pos_x] == FLOOR)
+		c->player->pos_y -= -c->player->dir_x * mv_speed;
 }
