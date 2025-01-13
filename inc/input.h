d5501b8144eb19ffa1f9ea95bcc325e3fb20aaba
outsiderice <arantxamagnelli@gmail.com>
2025-01-13 11:43:02 +0100

    added delta time to rotation

--- inc/input.h
+++ inc/input.h
@@ -3,7 +3,7 @@
 
 #include "render.h"
 
-# define ROT_ANGLE 0.1
+# define ROT_ANGLE 3.0
 
 void	close_cub(t_cub *cub);
 void	ft_hook(void *param);
--- src/input.c
+++ src/input.c
@@ -17,30 +17,48 @@ void	close_cub(t_cub *cub)
 	exit(EXIT_SUCCESS);
 }
 
-void	rotate_left(double x, double y, double *new_x, double *new_y)
+void	rotate_right(t_cub *c, double x, double y)
 {
-	*new_x = x * cos(ROT_ANGLE) - y * sin(ROT_ANGLE);
-	*new_y = x * sin(ROT_ANGLE) + y * cos(ROT_ANGLE);
+	double	rot_spd;
+	double	p_x;
+	double	p_y;
+
+	p_x = c->player->plane_x;
+	p_y = c->player->plane_y;
+	rot_spd = c->mlx->delta_time * ROT_ANGLE;
+	c->player->dir_x = x * cos(rot_spd) - y * sin(rot_spd);
+	c->player->dir_y = x * sin(rot_spd) + y * cos(rot_spd);
+	c->player->plane_x = p_x * cos(rot_spd) - p_y * sin(rot_spd);
+	c->player->plane_y = p_x * sin(rot_spd) + p_y * cos(rot_spd);
 }
 
-void	rotate_right(double x, double y, double *new_x, double *new_y)
+void	rotate_left(t_cub *c, double x, double y)
 {
-	*new_x = x * cos(-ROT_ANGLE) - y * sin(-ROT_ANGLE);
-	*new_y = x * sin(-ROT_ANGLE) + y * cos(-ROT_ANGLE);
+	double	rot_spd;
+	double	p_x;
+	double	p_y;
+
+	p_x = c->player->plane_x;
+	p_y = c->player->plane_y;
+	rot_spd = c->mlx->delta_time * ROT_ANGLE;
+	c->player->dir_x = x * cos(-rot_spd) - y * sin(-rot_spd);
+	c->player->dir_y = x * sin(-rot_spd) + y * cos(-rot_spd);
+	c->player->plane_x = p_x * cos(-rot_spd) - p_y * sin(-rot_spd);
+	c->player->plane_y = p_x * sin(-rot_spd) + p_y * cos(-rot_spd);
 }
 
-void	update_player_dir(t_player *player, int key)
+void	update_player_dir(t_cub *c, int key)
 {
 	if (key == 'R')
 	{
-		rotate_left(player->dir_x, player->dir_y, &player->dir_x, &player->dir_y);
-		rotate_left(player->plane_x, player->plane_y, &player->plane_x, &player->plane_y);
+		rotate_right(c, c->player->dir_x, c->player->dir_y);
 	}
 	else
 	{
-		rotate_right(player->dir_x, player->dir_y, &player->dir_x, &player->dir_y);
-		rotate_right(player->plane_x, player->plane_y, &player->plane_x, &player->plane_y);
+		rotate_left(c, c->player->dir_x, c->player->dir_y);
 	}
+	printf("After Rotation: dir_x=%f, dir_y=%f, plane_x=%f, plane_y=%f\n",
+           c->player->dir_x, c->player->dir_y, c->player->plane_x, c->player->plane_y);
 }
 
 void	update_player_pos(t_cub *cub, int key)
@@ -74,6 +92,10 @@ void ft_hook(void *param)
 	c = param;
 	if (mlx_is_key_down(c->mlx, MLX_KEY_ESCAPE))
 		close_cub(c);
+	if (mlx_is_key_down(c->mlx, MLX_KEY_LEFT))
+		update_player_dir(c, 'L');
+	if (mlx_is_key_down(c->mlx, MLX_KEY_RIGHT))
+		update_player_dir(c, 'R');
 	if (mlx_is_key_down(c->mlx, MLX_KEY_W))
 		update_player_pos(c, 'W');
 	if (mlx_is_key_down(c->mlx, MLX_KEY_S))
@@ -82,9 +104,5 @@ void ft_hook(void *param)
 		update_player_pos(c, 'A');
 	if (mlx_is_key_down(c->mlx, MLX_KEY_D))
 		update_player_pos(c, 'D');
-	if (mlx_is_key_down(c->mlx, MLX_KEY_LEFT))
-		update_player_dir(c->player, 'L');
-	if (mlx_is_key_down(c->mlx, MLX_KEY_RIGHT))
-		update_player_dir(c->player, 'R');
 	render(c);
 }	
