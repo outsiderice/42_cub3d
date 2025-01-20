a7e2d9de3abbf91c4cb141a4ac07a554d4437f1e
A.M.S <99796232+outsiderice@users.noreply.github.com>
2025-01-20 17:27:18 +0000

    texture coordinate

--- src/textures.c
+++ src/textures.c
@@ -1,10 +1,37 @@
 #include "assets.h"
 
-void	render_texture(t_cub *c)
-{}
+/* TO do
+	-add ray dirs to t_raycast
+	-
+*/
 
-//calculate x coordinate where the ray hits the wall
-void	wall_coordinate(t_raycast r, t_player *p, double ray_x, double ray_y)
+void	render_texture(t_cub *c)
+{
+
+}
+
+int	texture_coordinate(t_raycast r, double ray_x, double ray_y, t_ass a, double wall_x)
+{
+	int	tex_x;
+	int	width;
+
+	if (r.wall_type == 'E')
+		width = a.e->width;
+	else if (r.wall_type == 'N')
+		width = a.n->width;
+	else if (r.wall_type == 'W')
+		width = a.w->width;
+	else
+		width = a.s->width;
+	tex_x = (int)(wall_x * width);
+	if (r.side == 0 && ray_x > 0)
+		tex_x = width - tex_x - 1;
+	if (r.side == 1 && ray_y < 0)
+		tex_x = width - tex_x - 1;
+}
+
+//calculate where exactly the ray hits the wall
+double	wall_coordinate(t_raycast r, t_player *p, double ray_x, double ray_y)
 {
 	double	wall_x;
 	
@@ -13,6 +40,7 @@ void	wall_coordinate(t_raycast r, t_player *p, double ray_x, double ray_y)
 	else
 		wall_x = p->pos_x + r.perp_wall_dist * ray_x;
 	wall_x -= floor(wall_x);
+	return (wall_x);
 }
 
 //loads png and sets textures and colors to asset struct
