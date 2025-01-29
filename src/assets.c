0ac8f489142da9f0f0938bfabe89e3b6dc2ae571
A.M.S <99796232+outsiderice@users.noreply.github.com>
2025-01-29 07:43:38 +0000

    moved set assets

---
+++ src/assets.c
@@ -0,0 +1,16 @@
+#include "assets.h"
+
+//loads png and sets textures and colors to asset struct
+//texture to image?? told might be leak otherwise, come back later if needed.
+t_ass	set_assets(t_map_info info)
+{
+	t_ass			ass;
+
+	ass.e = mlx_load_png(info.east);
+	ass.w = mlx_load_png(info.west);
+	ass.n = mlx_load_png(info.north);
+	ass.s = mlx_load_png(info.south);
+	ass.c_color = get_rgba(info.ceiling[0], info.ceiling[1], info.ceiling[2], 255);
+	ass.f_color = get_rgba(info.floor[0], info.floor[1], info.floor[2], 255);
+	return (ass);
+}
